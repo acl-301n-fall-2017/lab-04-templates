6e0ebd39385cbfa7770b96928573aa0449aafa1c
William Tronvig <wdtronvig289840@gmail.com>
2018-05-03 19:54:49 -0700

    completed the lab

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,9 +12,12 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // TODONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
+
+  var template = $('#handy-template').html();
+  var compile = Handlebars.compile(template);
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
@@ -23,8 +26,8 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  // TODONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  $('main').append(compile(this));
 };
 
 rawData.sort(function(a,b) {
